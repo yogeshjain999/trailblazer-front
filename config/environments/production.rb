2aaacce196ebdf1cda8c1cafdc24e7f9f1b07af7
Nick Sutterer <apotonick@gmail.com>
2023-08-09 12:09:14 +0200

    add secret key.

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -2,6 +2,7 @@ require "active_support/core_ext/integer/time"
 
 Rails.application.configure do
   config.hosts << "website-mia.onrender.com"
+  config.secret_key_base = "asdf"
 
   # Settings specified here will take precedence over those in config/application.rb.
 
