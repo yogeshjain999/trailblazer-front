4eee921b26a170a6f9bbf54ae20bac4c53c68407
maciek <maciek.sokolowskii@gmail.com>
2023-08-22 13:58:41 +0200

    add highlight js and add example ruby code snippet

--- app/javascript/application.js
+++ app/javascript/application.js
@@ -1,2 +1,7 @@
 // Configure your import map in config/importmap.rb. Read more: https://github.com/rails/importmap-rails
 import "./navigations"
+import hljs from 'highlight.js/lib/core';
+import ruby from 'highlight.js/lib/languages/ruby';
+
+hljs.registerLanguage("ruby", ruby)
+hljs.highlightAll();
