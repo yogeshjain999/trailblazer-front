309be3d64060d4f6a05cf200a17522de502e30d9
maciek <maciek.sokolowskii@gmail.com>
2023-08-22 16:15:48 +0200

    Add TW principles to follow and remove some @apply directives

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,5 @@
 module ApplicationHelper
+  def navbar_link_to(text, path)
+    link_to text, path, class: "font-medium text-base uppercase hover:scale-110 lg:normal-case lg:font-semibold"
+  end
 end
