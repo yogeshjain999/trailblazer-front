2b7929207e6efe076692b5f65b7ad10b9d53d205
Nick Sutterer <apotonick@gmail.com>
2023-08-29 15:48:43 +0200

    left menu is almost working. page headers.

--- app/controllers/views_controller.rb
+++ app/controllers/views_controller.rb
@@ -43,7 +43,12 @@ class ViewsController < ApplicationController
           }
         end
 
+        def code_tabs(*args, **kws) # TODO: implement tabs
+          code(*args, **kws)
+        end
+        def api(*) # FIXME: implement
 
+        end
 
 
         def vite_image_tag(*)
@@ -100,8 +105,8 @@ class ViewsController < ApplicationController
         %(<a href="" class="#{options[:class]}">#{text}</a>)
       end
 
-      def initialize(left_toc_html:)
-        @options = {left_toc_html: left_toc_html}
+      def initialize(left_toc_html:, version_options:)
+        @options = {left_toc_html: left_toc_html, documentation_title: version_options[:title]||raise }
       end
 
       def to_h
@@ -110,6 +115,10 @@ class ViewsController < ApplicationController
 
       def toc_left
         @options[:left_toc_html]
+      end
+
+      def documentation_title
+        @options[:documentation_title]
       end
     end
 
@@ -135,42 +144,70 @@ class ViewsController < ApplicationController
     layout_options = {cell: doc_layout_cell, template: doc_layout_template, left_toc: {cell: left_toc, template: left_toc_template}}
 
     pages = {
-      # "cells" => {
-      #   title: "Cells",
-      #   "4.0" => { # "prefix/version"
-      #     snippet_dir: "test/cells/",
-      #     section_dir: "test/sections/cells/4.0",
-      #     target_file: "test/site/2.1/docs/cells/index.html",
-      #     "overview.md.erb" => { snippet_file: "cell_test.rb" }
-      #   },
-      #   "5.0" => {
-      #     snippet_dir: "test/cells-5/",
-      #     section_dir: "test/sections/cells/5.0",
-      #     target_file: "test/site/2.1/docs/cells/5.0/index.html",
-      #   }
-      # },
-      # "reform" => {
-      #   title: "Reform",
-      #   "2.3" => {
-      #     snippet_dir: "test/code/reform",
-      #     section_dir: "test/sections/reform",
-      #     target_file: "test/site/2.1/docs/reform/index.html",
-      #     "intro.md.erb" => { snippet_file: "intro_test.rb" }
-      #   }
-      # }
       "activity" => {
-        title: "Activity",
+        toc_title: "Activity",
         "2.1" => {
+          title: "Activity",
           snippet_dir: "../trailblazer-activity-dsl-linear/test/docs",
           section_dir: "../website-NEW/app/concepts/documentation/page/snippet/activity",
           target_file: "tmp/activity.html",
           target_url:  "/2.1/docs/activity/index.html",
           layout:      layout_options,
 
+          "activity.md.erb" => { snippet_file: "basics_test.rb" },
+          "dsl/strategy.md.erb" => { snippet_file: "strategy_test.rb" },
+          "dsl/api.md.erb" => { snippet_file: "basics_test.rb" },
+          "dsl/path.md.erb" => { snippet_file: "path_test.rb" },
+          "dsl/subprocess.md.erb" => { snippet_file: "subprocess_test.rb" },
+          "dsl/options.md.erb" => { snippet_file: "subprocess_test.rb" },
+          "dsl/sequence.md.erb" => { snippet_file: "sequence_options_test.rb" },
+          "dsl/patching.md.erb" => { snippet_file: "patching_test.rb" },
+          "dsl/composable_variable_mapping.md.erb" => { snippet_file: "composable_variable_mapping_test.rb" },
+          "dsl/variable_mapping.md.erb" => { snippet_file: "variable_mapping_test.rb" },
+          "dsl/macro.md.erb" => { snippet_file: "macro_test.rb" },
+          "internals.md.erb" => { snippet_file: "macro_test.rb" },
+          "internals/introspect.md.erb" => { snippet_file: "introspect_test.rb" },
+          "interfaces.md.erb" => { snippet_file: "activity_test.rb" },
           "task_wrap.md.erb" => { snippet_file: "task_wrap_test.rb" },
+          "troubleshooting.md.erb" => {section_dir: "../website-NEW/app/concepts/documentation/page/snippet/developer", snippet_dir: "../trailblazer-developer/test/docs", snippet_file: "developer_test.rb" },
           "kitchen_sink.md.erb" => { snippet_file: "____test.rb" },
         }
+      },
+
+      "macro" => {
+        toc_title: "Macro",
+        "2.1" => {
+          title: "Macro",
+          snippet_dir: "../trailblazer-macro/test/docs",
+          section_dir: "../website-NEW/app/concepts/documentation/page/snippet/macro",
+          target_file: "tmp/macro.html",
+          target_url: "/2.1/docs/macro/index.html",
+          layout: layout_options,
+
+          "overview.md.erb"   => {snippet_file: "model_test.rb"},
+          "nested/dynamic.md.erb"   => {snippet_file: "nested_static_test.rb"},
+          "nested/auto_wire.md.erb"   => {snippet_file: "nested_static_test.rb"},
+          "wrap.md.erb"   => {snippet_file: "wrap_test.rb"},
+          "each.md.erb"   => {snippet_file: "each_test.rb"},
+          "model_find.md.erb"   => {snippet_file: "model/find_test.rb"},
+          "model.md.erb"   => {snippet_file: "model_test.rb"},
+          "rescue.md.erb"   => {snippet_file: "rescue_test.rb"},
+          "policy.md.erb"   => {snippet_file: "policy_test.rb"},
+        }
+      },
+
+      "operation" => {
+        toc_title: "Operation",
+        "2.1" => {
+          title: "Operation",
+          snippet_dir: "../trailblazer-operation/test/docs",
+          section_dir: "../website-NEW/app/concepts/documentation/page/snippet/operation",
+          target_file: "tmp/operation.html",
+          target_url: "/2.1/docs/operation/index.html",
+          layout: layout_options,
+        }
       }
+
     }
 
 
