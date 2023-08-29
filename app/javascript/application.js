5663d7d1b1534b4e4605344bf3f96cdd054a2373
Yogesh Khater <yogeshjain999@gmail.com>
2023-08-30 00:08:54 +0530

    Add anchor-js and make right toc sticky

--- app/javascript/application.js
+++ app/javascript/application.js
@@ -1,2 +1,19 @@
 // Configure your import map in config/importmap.rb. Read more: https://github.com/rails/importmap-rails
-import "./navigations"
+import anchorJS from 'anchor-js';
+import ScrollMagic from 'scrollmagic';
+
+import './navigations';
+
+document.addEventListener('DOMContentLoaded', function () {
+  var anchors = new anchorJS();
+  anchors.add();
+
+  var controller = new ScrollMagic.Controller();
+
+  new ScrollMagic.Scene({
+    offset: 145,
+    duration: '200%'
+  })
+    .setPin('#right-toc') // pins the element for the the scene's duration
+    .addTo(controller);
+});
--- config/importmap.rb
+++ config/importmap.rb
@@ -1,3 +1,5 @@
 # Pin npm packages by running ./bin/importmap
 
 pin "application", preload: true
+pin "anchor-js", preload: true # @5.0.0
+pin "scrollmagic", preload: true # @2.0.8
