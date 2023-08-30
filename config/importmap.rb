35943310e751bcdd18c16d6c55b7f1c89dc0147f
Nick Sutterer <apotonick@gmail.com>
2023-08-30 11:22:34 +0200

    retrieve highlightjs from CDN until we fix the local.

--- config/importmap.rb
+++ config/importmap.rb
@@ -3,5 +3,7 @@
 pin "application", preload: true
 pin "anchor-js", preload: true # @5.0.0
 pin "scrollmagic", preload: true # @2.0.8
-pin "highlight.js/lib/core", to: "highlight.js--lib--core.js" # @11.8.0
-pin "highlight.js/lib/languages/ruby", to: "highlight.js--lib--languages--ruby.js" # @11.8.0
+# pin "highlight.js/lib/core", to: "highlight.js--lib--core.js" # @11.8.0
+# pin "highlight.js/lib/languages/ruby", to: "highlight.js--lib--languages--ruby.js" # @11.8.0
+pin "highlight.js/lib/core", to: "https://ga.jspm.io/npm:highlight.js@11.8.0/es/core.js"
+pin "highlight.js/lib/languages/ruby", to: "https://cdnjs.cloudflare.com/ajax/libs/highlight.js/11.8.0/es/languages/ruby.min.js"
