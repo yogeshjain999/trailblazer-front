c3c088a60c9e906f9143b0cccd2c13cd6df50045
Nick Sutterer <apotonick@gmail.com>
2023-09-01 07:39:04 +0200

    fix menus.

--- config/tailwind.config.js
+++ config/tailwind.config.js
@@ -5,7 +5,8 @@ module.exports = {
     "./public/*.html",
     "./app/helpers/**/*.rb",
     "./app/javascript/**/*.js",
-    "./app/views/**/*.{erb,haml,html,slim}"
+    "./app/views/**/*.{erb,haml,html,slim}",
+    "./app/concepts/**/*.{erb,haml,html,slim}"
   ],
   theme: {
     colors: {
