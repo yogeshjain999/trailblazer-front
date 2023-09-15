48fab4733fce4032ef61c9d4cc348e351b1f3c0d
maciek <maciek.sokolowskii@gmail.com>
2023-09-15 14:03:17 +0200

    landing page prototype

--- config/importmap.rb
+++ config/importmap.rb
@@ -9,4 +9,3 @@ pin "navigations"
 # pin "highlight.js/lib/languages/ruby", to: "highlight.js--lib--languages--ruby.js" # @11.8.0
 pin "highlight.js/lib/core", to: "https://ga.jspm.io/npm:highlight.js@11.8.0/es/core.js"
 pin "highlight.js/lib/languages/ruby", to: "https://cdnjs.cloudflare.com/ajax/libs/highlight.js/11.8.0/es/languages/ruby.min.js"
-
