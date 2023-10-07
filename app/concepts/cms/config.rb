a94b75ce2b77a5cbd6ce5c8a15d8c32235ce2617
Yogesh Khater <yogeshjain999@gmail.com>
2023-10-07 21:26:53 +0530

    Fix spacing

--- app/concepts/cms/config.rb
+++ app/concepts/cms/config.rb
@@ -6,9 +6,9 @@ module Cms
       pre: {class: ""},
       code: {class: "rounded"},
       codespan: {class: "text-purple"},
-      h2: {class: "text-2xl font-bold text-neutral-500 lg:text-3xl"}, # is from spacing/15 in tailwind.config.js.
-      h3: {class: "font-bold text-neutral-500 lg:text-2xl text-xl"}, # is from spacing/15 in tailwind.config.js.
-      h4: {class: "font-bold text-neutral-500 lg:text-1xl text-xl"}, # mt-15 is from spacing/15 in tailwind.config.js.
+      h2: {class: "text-2xl font-bold text-neutral-500 lg:text-3xl py-2"}, # is from spacing/15 in tailwind.config.js.
+      h3: {class: "font-bold text-neutral-500 lg:text-2xl text-xl py-2"}, # is from spacing/15 in tailwind.config.js.
+      h4: {class: "font-bold text-neutral-500 lg:text-1xl text-xl py-2"}, # mt-15 is from spacing/15 in tailwind.config.js.
       a: {class: "underline text-purple"},
       img: {class: "mt-12 mb-12 mx-auto"},
     ).freeze
