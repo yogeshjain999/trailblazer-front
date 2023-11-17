9f9537f582107be91a1644ad65e8b6878ced6e41
Nick Sutterer <apotonick@gmail.com>
2023-11-17 16:34:50 +0100

    allow gem_version embed in h3.

--- app/concepts/cms/config.rb
+++ app/concepts/cms/config.rb
@@ -7,7 +7,7 @@ module Cms
       code: {class: "rounded"},
       codespan: {class: "text-purple"},
       h2: {class: "text-2xl font-bold text-neutral-500 lg:text-3xl py-2 flex items-center"}, # is from spacing/15 in tailwind.config.js.
-      h3: {class: "font-bold text-neutral-500 lg:text-2xl text-xl py-2"}, # is from spacing/15 in tailwind.config.js.
+      h3: {class: "font-bold text-neutral-500 lg:text-2xl text-xl py-2 flex items-center"}, # is from spacing/15 in tailwind.config.js.
       h4: {class: "font-bold text-neutral-500 lg:text-1xl text-xl py-2"}, # mt-15 is from spacing/15 in tailwind.config.js.
       a: {class: "underline text-purple"},
       img: {class: "mt-12 mb-12 mx-auto"},
