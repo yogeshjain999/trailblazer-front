bba365994434753fb16ac6d5307c08e1b07cde75
Nick Sutterer <apotonick@gmail.com>
2023-11-30 11:31:53 +0100

    serve highlightjs locally.

--- config/importmap.rb
+++ config/importmap.rb
@@ -3,10 +3,10 @@
 pin "application", preload: true
 pin "anchor-js", preload: true # @5.0.0
 pin "navigations"
-# pin "highlight.js/lib/core", to: "highlight.js--lib--core.js" # @11.8.0
-# pin "highlight.js/lib/languages/ruby", to: "highlight.js--lib--languages--ruby.js" # @11.8.0
-pin "highlight.js/lib/core", to: "https://ga.jspm.io/npm:highlight.js@11.8.0/es/core.js"
-pin "highlight.js/lib/languages/ruby", to: "https://cdnjs.cloudflare.com/ajax/libs/highlight.js/11.8.0/es/languages/ruby.min.js"
-pin "jquery" # @3.7.1
 
+# bin/importmap pin --download highlight.js/lib/core DOESN'T WORK I downloaded highlight.js/lib/core.js manually and put it in vendor with the --ed name
+#                              highlight.js/lib/languages/ruby
+pin "highlight.js/lib/core", to: "highlight.js--lib--core.js" # @11.9.0
+pin "highlight.js/lib/languages/ruby", to: "highlight.js--lib--languages--ruby.js" # @11.9.0
+pin "jquery" # @3.7.1
 pin "jquery.parallax-scroll"
