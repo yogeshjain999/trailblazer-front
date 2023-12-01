415143c6794f237599c58de3c12144d08db8e240
Nick Sutterer <apotonick@gmail.com>
2023-12-01 09:52:22 +0100

    Publishing dist/

--- dist/assets/application-90866b021f1d489bb3e490a5ee4b51a4ee0b9287d72a7fceea3e4b7c68e1693f.js
+++ dist/assets/application-90866b021f1d489bb3e490a5ee4b51a4ee0b9287d72a7fceea3e4b7c68e1693f.js
@@ -3,6 +3,7 @@ import ruby from 'highlight.js/lib/languages/ruby';
 import anchorJS from 'anchor-js';
 
 import 'navigations'; // FIXME: here, part of the code should only be run when "docs".
+import './docsearch';
 
 import jquery from 'jquery';
 
---
+++ dist/assets/docsearch-cea68a0a1429b11b747954b90c49d36b8f211dbea62e82d79ff0c7bef5226dc2.js
@@ -0,0 +1,8 @@
+import '@docsearch/js';
+
+docsearch({
+  container: '#docsearch',
+  appId: 'LEN50BR1UK',
+  indexName: 'trailblazer-website-24',
+  apiKey: 'b1f311e280db1c40850ee82e08eb3449'
+});
