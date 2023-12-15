dc12ec6d696ec9e83c1953db6f420c5c3eecd37a
Nick Sutterer <apotonick@gmail.com>
2023-12-15 08:16:23 +0100

    Publishing dist/

--- dist/assets/docsearch-cea68a0a1429b11b747954b90c49d36b8f211dbea62e82d79ff0c7bef5226dc2.js
+++
@@ -1,8 +0,0 @@
-import '@docsearch/js';
-
-docsearch({
-  container: '#docsearch',
-  appId: 'LEN50BR1UK',
-  indexName: 'trailblazer-website-24',
-  apiKey: 'b1f311e280db1c40850ee82e08eb3449'
-});
---
+++ dist/assets/docsearch-ec78641e626432933b5c9f60c1f9de8cf1c15c4446aa565f6b185495eed4cdb0.js
@@ -0,0 +1,20 @@
+import '@docsearch/js';
+
+// TODO: remove, currently unused function.
+function my_transformItems(items) {
+    return items.map((item) => {
+      return {
+        ...item,
+        hierarchy: {...item.hierarchy, lvl1: "yo yo yo yo"},
+      }
+    })
+  }
+
+docsearch({
+  container: '#docsearch',
+  appId: 'LEN50BR1UK',
+  indexName: 'trailblazer-website-24',
+  apiKey: 'b1f311e280db1c40850ee82e08eb3449',
+
+  // transformItems: my_transformItems,
+});
